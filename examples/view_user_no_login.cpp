ff27ae7388332788fac5bd023f10d52e9cb925d5
_ <thomash1996@live.com>
2025-01-20 10:57:58 -0600

    added suspended printout

--- examples/view_user_no_login.cpp
+++ examples/view_user_no_login.cpp
@@ -7,13 +7,17 @@
 
 int main() {
     // we will get the Admin user C
-    nlohmann::json user = ScoredCoApi::getUser("C");
+    nlohmann::json user = ScoredCoApi::getUser("c");
 
     // view json blob in terminal 
     // std::cout << user << std::endl;
 
     // user will be empty if none exists
     if (user != nullptr && !user.empty()) {
+        if (user.value("is_suspended", false)) {
+            std::cout << "This user is suspended." << std::endl;
+        }
+
         std::cout << "A list of the first ten communities user " << user.value("username", "") << " moderates:" << std::endl;
 
         int i = 0;
